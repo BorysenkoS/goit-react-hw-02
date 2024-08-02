1c5aa30dcd58658a4366649174c855b3c46355c8
Serhii Borysenko <borisenko37@gmail.com>
2024-08-02 12:26:08 +0200

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import "./App.css";
 import Description from "./components/Description/Description";
 import Options from "./components/Options/Options";
-import Feedback from "./components/Feedback/Feedback";
 import Notification from "./components/Notification/Notification";
+import Feedback from "./components/Feedback/Feedback";
 import { useEffect, useState } from "react";
 
 function App() {
